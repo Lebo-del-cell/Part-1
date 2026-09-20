29536f7bffbcfef514c9a60849abb054a79a3ad0
Lebo-del-cell <ST10524958@rcconnect.edu.za>
2026-09-20 15:28:02 +0200

    Add console registration and login application

--- src/Login.java
+++ src/Login.java
@@ -40,7 +40,7 @@ public class Login {
 
         }
          
-        if (hasCapitalLetter && hasNumber && hasSpecialCharacter){return true;
+        if(hasCapitalLetter && hasNumber && hasSpecialCharacter){return true;
         } else {
             return false;
         }
@@ -79,7 +79,7 @@ public class Login {
     }
     // login for User
     public boolean loginUser (String username, String password) {
-        if (this.username !=null && this.password !=null && this.username.equals(username) && this.password.equals(password)) {
+        if(this.username !=null && this.password !=null && this.username.equals(username) && this.password.equals(password)) {
             return true;
         }else{
     return false;
